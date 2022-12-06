e8171ad17c4dff2165deece6cecaa72ea66e8d7b
Glaucielle Sá <65506911+glauciellesa@users.noreply.github.com>
2022-12-06 15:16:15 +0100

    Refactor components organization

---
+++ src/components/tabela/AcoesCliente.tsx
@@ -0,0 +1,38 @@
+import Cliente from "../../core/Cliente";
+import { IconeDeletar, IconeEditar } from "../Icones";
+
+interface AcoesClienteProps {
+  cliente: Cliente;
+  clienteSelecionado?: (cliente: Cliente) => void; //void is an operator, which is used when the function is not return anything.
+  clienteExcluido?: (cliente: Cliente) => void;
+}
+export default (props: AcoesClienteProps) => {
+  /* console.log(props.exibirAcoes);
+   */
+  return (
+    <td className="flex justify-center">
+      {props.clienteSelecionado ? (
+        <button
+          onClick={() => props.clienteSelecionado?.(props.cliente)} // _____?.() = opcional chain
+          className={`first-letter:flex justify-center items-center
+          text-green-700 rounded-full p-2 m-1 hover:bg-red-50`}
+        >
+          {IconeEditar}
+        </button>
+      ) : (
+        false
+      )}
+      {props.clienteExcluido ? (
+        <button
+          onClick={() => props.clienteExcluido?.(props.cliente)} // _____?.() = opcional chain
+          className={`first-letter:flex justify-center items-center
+             text-red-700 rounded-full p-2 m-1 hover:bg-green-50`}
+        >
+          {IconeDeletar}
+        </button>
+      ) : (
+        false
+      )}
+    </td>
+  );
+};
